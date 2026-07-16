a2730d4de077d3c813daf34ec1992dc59275a9e3
SH20FK <sh20fk@users.noreply.github.com>
2026-07-16 12:27:43 +0500

    fix: use GameRule.set(GameRules, value, server) directly

--- src/main/java/com/project3/world/CalibrationManager.java
+++ src/main/java/com/project3/world/CalibrationManager.java
@@ -61,7 +61,7 @@ public final class CalibrationManager {
                     border.interpolateSize(border.getSize(), 42000.0, 2000000L, net.minecraft.util.Util.getMeasuringTimeMs());
                     
                     // Disable vanilla advancement announcements
-                    net.minecraft.world.rule.GameRule.ANNOUNCE_ADVANCEMENTS.getValue(overworld.getGameRules()).set(false, server);
+                    net.minecraft.world.rule.GameRules.ANNOUNCE_ADVANCEMENTS.set(overworld.getGameRules(), false, server);
                 }
             }
         }
