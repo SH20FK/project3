409bed84de014f060bbdffd36d16a5c9a128ebb8
SH20FK <sh20fk@users.noreply.github.com>
2026-07-16 12:42:05 +0500

    fix: renderSky signature changed in 1.21.11 - 2nd Matrix4f replaced by Fog

--- src/client/java/com/project3/mixin/MixinWorldRendererSky.java
+++ src/client/java/com/project3/mixin/MixinWorldRendererSky.java
@@ -13,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(WorldRenderer.class)
 public class MixinWorldRendererSky {
 
-    @Inject(method = "renderSky(Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At("HEAD"), cancellable = true)
-    private void onRenderSky(org.joml.Matrix4f matrix4f, org.joml.Matrix4f matrix4f2, float f, net.minecraft.client.render.Camera camera, boolean bl, Runnable runnable, CallbackInfo ci) {
+    @Inject(method = "renderSky(Lorg/joml/Matrix4f;Lnet/minecraft/client/render/Fog;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At("HEAD"), cancellable = true)
+    private void onRenderSky(org.joml.Matrix4f matrix4f, net.minecraft.client.render.Fog fog, float f, net.minecraft.client.render.Camera camera, boolean bl, Runnable runnable, CallbackInfo ci) {
         if (GloomVoidClientHandler.isInGloomVoid) {
             // Cancel vanilla sky rendering (Sun, Moon, Stars, Colors)
             // The background clear color is already dark from MixinFogRenderer, 
